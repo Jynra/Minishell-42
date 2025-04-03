2d16f53e626c839f7cb8b194af4955fe940cbf7a
Eliot Lucas <ellucas@c1r5s3.42lausanne.ch>
2025-04-03 16:14:39 +0200

    des

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ellucas <ellucas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 14:28:14 by ellucas           #+#    #+#             */
-/*   Updated: 2025/04/03 15:44:10 by ellucas          ###   ########.fr       */
+/*   Updated: 2025/04/03 16:14:29 by ellucas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,22 @@
 # include <unistd.h>
 # include <sys/types.h>
 # include <signal.h>
+
+/* Structure */
+typedef struct s_pipex
+{
+	int	infile;
+	int	outfile;
+	int	pipe_fd[2];
+	char	**cmd1_args;
+	char	**cmd2_args;
+	char	*cmd1_path;
+	char	*cmd2_path;
+	char	**envp;
+}	t_pipex;
+
+/* Fonctions */
+void	errors(char	*str);
 
 
 #endif
